3b28f95ae57a51be2596b0365b51734bb04947f0
Aaron Muesing <muesing.music@gmail.com>
2021-01-07 23:03:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
 final results = data.each_with_object ({}) do | (key, value), final_array |
-  value.each do 
+  value.each do | inner_names, key |
 end
