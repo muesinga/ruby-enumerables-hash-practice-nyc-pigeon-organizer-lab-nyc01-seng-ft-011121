fed077f05719f987ef15fcb18965f6edacfc913c
Aaron Muesing <muesing.music@gmail.com>
2021-01-07 23:09:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,4 +9,8 @@ final results = data.each_with_object ({}) do | (key, value), final_array |
       if !final_array[name][key]
         !final_array[name][key] = []
       end
+    final_array[name][key].push[inner_key.to_s]
+  end
+end
+end
 end
