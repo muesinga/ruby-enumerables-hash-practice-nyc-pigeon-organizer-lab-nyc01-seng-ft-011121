2ef1ac299d541ec5800bde83cc1f8446c528b47e
Aaron Muesing <muesing.music@gmail.com>
2021-01-07 21:43:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,5 @@
+require 'Pry'
+
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  Theo = []
-  Peter_Jr = []
-  Lucky = []
-  Ms_K = []
-  Queenie = []
-  Andrew = []
-  Alex = []
-    data.find_all { | color |
+binding.Pry
 end
